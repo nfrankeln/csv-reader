3d362b5426af3a57063d65e63c92bacc52e36ac1
nfrankeln <ntnyfranky@gmail.com>
2022-10-12 13:55:02 -0500

    wrote animal lookup function

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import csv
 def animals_lookup():
     animal_to_lookup=input("What animals are you looking for? Ex: \"cats,dogs?\": ")
     try: 
-        f = open(f'week2/csv-reader/data/{animal_to_lookup}.csv', mode='r')
+        selected_file = open(f'week2/csv-reader/data/{animal_to_lookup}.csv', mode='r')
         user_input=True
     except:
         print(f'Sorry we don\'t have {animal_to_lookup} at our rescue Shelter')
         user_input=False
+    
     if user_input:
-        print(f.read())
-        f.close()
+        dict=csv.DictReader(selected_file)
+        print(f'Here are the {animal_to_lookup} we have for adoption')
+        for row in dict:
+            print(f'{row["name"]} is a {row[" age"]} year old {row[" breed"]}')
+
 print(animals_lookup())
 
